92d7bef9cd1acf2083e953bf322e2d5d9596a128
talSofer <talsof@gmail.com>
2016-05-19 13:47:03 +0300

    cm

--- imports/ui/pages/AutomaticQuestionsPage.jsx
+++ imports/ui/pages/AutomaticQuestionsPage.jsx
@@ -1,3 +1,36 @@
 /**
  * Created by talSofer on 5/19/16.
  */
+
+import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import Snackbar from 'material-ui/Snackbar';
+import FreeTextQuestion from '../components/FreeTextQuestion.jsx';
+import MulChoiseQuestion from '../components/MulChoiseQuestion.jsx';
+import YesNoQuestion from '../components/YesNoQuestion.jsx';
+
+
+export default class AutomaticQuestionsPage extends React.Component{
+    constructor(props){
+        super(props);
+
+
+        this.handleDelete = this.handleDelete.bind(this);
+
+    }
+
+    getChildContext() {
+        return {muiTheme: getMuiTheme()};
+    }
+
+
+    render(){
+        const yesNoQuestions = this.props.yesNoQues.map((question)=>{
+            return (
+                <YesNoQuestion key={question._id} question={question} handleChangeState={this.handleDelete}/>
+            )
+        });
+    }
+}
+
+AutomaticQuestionsPage.childContextTypes = {muiTheme: React.PropTypes.object.isRequired};
